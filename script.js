e53f0437699b72353a091743f471ccfb957dca2b
Arnav Saha <sahaarnav3@gmail.com>
2024-04-02 20:10:32 +0530

    initial commit..

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".favourites").addEventListener("click", () => {
+    console.log("lau");
+    window.location.href = 'favourites.html';
+})
+document.querySelector(".logo").addEventListener("click", () => {
+    console.log("lau");
+    window.location.href = 'index.html';
+})
