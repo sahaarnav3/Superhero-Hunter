3ef8ac924553ef4be28a71f0626a71a6d7f021c8
Arnav Saha <sahaarnav3@gmail.com>
2024-04-04 12:34:16 +0530

    A basic layout of home page is finished..

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.querySelector(".favourites").addEventListener("click", () => {
-    console.log("lau");
     window.location.href = 'favourites.html';
 })
 document.querySelector(".logo").addEventListener("click", () => {
-    console.log("lau");
     window.location.href = 'index.html';
+})
+document.querySelector(".logo2").addEventListener("click", () => {
+    window.location.href = 'index.html';
+})
+document.querySelector(".start-button").addEventListener("click", () => {
+    document.querySelector(".over-main").style.left = 0;
+    document.querySelector(".start-button").classList.add("hidden");
+    document.querySelector(".background").style.filter = "blur(7px)";
 })
